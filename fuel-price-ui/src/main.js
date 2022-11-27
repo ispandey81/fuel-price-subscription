e240308bdd970d43f5afa5ea1446dfa8bc43fde4
indra <ispandey81@gmail.com>
2022-11-27 17:12:33 +0800

    Initial commit

---
+++ fuel-price-ui/src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.use(vuetify);
+
+app.mount("#app");
