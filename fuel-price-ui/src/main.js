edc0618979b3cc31aeb59a617608c42c0900457a
indra <ispandey81@gmail.com>
2022-11-28 17:26:49 +0800

    Added router, unsubscribe page and refactoring

--- fuel-price-ui/src/main.js
+++ fuel-price-ui/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from './router'
 
 import "./assets/main.css";
 
@@ -22,6 +23,8 @@ const vuetify = createVuetify({
 });
 
 const app = createApp(App);
+
+app.use(router);
 
 app.use(vuetify);
 
---
+++ fuel-price-ui/src/views/UnsubscribeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-center">
+    <p>Unsubscribe page yet to be built</p>
+  </div>
+</template>
